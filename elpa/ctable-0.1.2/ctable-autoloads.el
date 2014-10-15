99249cfb4ee251bb5db3b93910dce925fac88e21
Mathias De Wachter <mathias.dewachter@nuance.com>
2014-10-15 08:50:30 -0400

    added python stuff

---
+++ elpa/ctable-0.1.2/ctable-autoloads.el
@@ -0,0 +1,18 @@
+;;; ctable-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("ctable-pkg.el" "ctable.el") (21566 24342
+;;;;;;  564218 830000))
+
+;;;***
+
+(provide 'ctable-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ctable-autoloads.el ends here
--- init.el
+++ init.el
@@ -13,3 +13,19 @@
   (package-refresh-contents))
 
 (load "~/.emacs.d/user.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector [zenburn-bg zenburn-red zenburn-green zenburn-yellow zenburn-blue zenburn-magenta zenburn-cyan zenburn-fg])
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes (quote ("9e54a6ac0051987b4296e9276eecc5dfb67fdcd620191ee553f40a9b6d943e78" default)))
+ '(fci-rule-color "#2a2a2a"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
