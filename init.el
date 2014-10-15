706bbc9205bfe65a20ae40ad8328313099e9a150
Mathias De Wachter <mathias.dewachter@nuance.com>
2014-10-15 15:49:20 -0400

    theme changes

--- init.el
+++ init.el
@@ -23,8 +23,8 @@
  '(background-color "#ffffff")
  '(background-mode light)
  '(cursor-color "#ffff00")
- '(custom-enabled-themes (quote (light-blue)))
- '(custom-safe-themes (quote ("52588047a0fe3727e3cd8a90e76d7f078c9bd62c0b246324e557dfa5112e0d0c" "1157a4055504672be1df1232bed784ba575c60ab44d8e6c7b3800ae76b42f8bd" "7f1263c969f04a8e58f9441f4ba4d7fb1302243355cb9faecb55aec878a06ee9" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "9e54a6ac0051987b4296e9276eecc5dfb67fdcd620191ee553f40a9b6d943e78" default)))
+ '(custom-enabled-themes (quote (tsdh-dark)))
+ '(custom-safe-themes (quote ("fc986960dbea32d64750a50e3e19dc50491c1d2e6b5e3119df9e886ab59d69b1" "52588047a0fe3727e3cd8a90e76d7f078c9bd62c0b246324e557dfa5112e0d0c" "1157a4055504672be1df1232bed784ba575c60ab44d8e6c7b3800ae76b42f8bd" "7f1263c969f04a8e58f9441f4ba4d7fb1302243355cb9faecb55aec878a06ee9" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "9e54a6ac0051987b4296e9276eecc5dfb67fdcd620191ee553f40a9b6d943e78" default)))
  '(fci-rule-color "#2a2a2a")
  '(foreground-color "#ffff00"))
 (custom-set-faces
